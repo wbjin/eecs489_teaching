6391f949750f4844ce26dcfc216522b98bda3d3f
Wonbin Jin <wbjin@umich.edu>
2025-08-27 12:33:23 -0400

    Lab 1 demo

---
+++ lab1/client.cpp
@@ -0,0 +1,46 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define PORT 8080
+
+int main() {
+    // Make a socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sockfd == -1)  {
+        perror("error making socket");
+        exit(1);
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    struct hostent *host = gethostbyname("127.0.0.1");
+    if (host == NULL) {
+        perror("error gethostbyname");
+        exit(1);
+    }
+    memcpy(&addr.sin_addr, host->h_addr, host->h_length);
+    addr.sin_port = 8080; 
+
+    // Connect to the server
+    if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        perror("error connecting");
+        exit(1);
+    }
+
+    // Send a message
+    char message[] = "Hi server!";
+    if (send(sockfd, message, sizeof(message), 0) == -1) {
+        perror("send");
+        exit(1);
+    }
+
+    close(sockfd);
+    shutdown(sockfd, SHUT_RDWR);
+}
---
+++ lab1/server.cpp
@@ -0,0 +1,67 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define PORT 8080
+
+int main() {
+    // Make a socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sockfd == -1)  {
+        perror("error making socket");
+        exit(1);
+    }
+
+    // Option for allowing you to reuse socket
+    int yes {1};
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+        perror("error setsockopt");
+        exit(1);
+    }
+
+    // Bind socket to a port
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = PORT;
+    if (bind(sockfd, (sockaddr*)&addr, (socklen_t) sizeof(addr)) == -1) {
+        perror("error binding socket");
+        exit(1);
+    }
+
+    // Listen for incoming connections
+    if (listen(sockfd, 10) == -1) {
+        perror("error listening");
+        exit(1);
+    }
+
+    while (true) {
+        // Accept new connection
+        struct sockaddr_in connection;
+        socklen_t size = sizeof(connection);
+        int connectionfd = accept(sockfd, (struct sockaddr*)&connection, &size);
+
+        // Print what IP address connection is from
+        char s[INET6_ADDRSTRLEN];
+        printf("connetion from %s\n", inet_ntoa(connection.sin_addr));
+
+        // Receive message
+        char buf[1024];
+        int ret {};
+        if ((ret = recv(connectionfd, buf, sizeof(buf), 0)) == -1) {
+            perror("recv");
+            close(connectionfd);
+            continue;
+        }
+        buf[ret] = '\0';
+
+        printf("message: %s\n", buf);
+        close(connectionfd);
+    }
+
+    close(sockfd);
+}
